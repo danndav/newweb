1309f82fe73bcef06540dd882e2fd2dce89cfbce
dandave <danielimodoye@gmail.com>
2018-11-19 10:49:52 +0100

    slow scrollong  menu

--- js/app.js
+++ js/app.js
@@ -20,48 +20,15 @@ $(document).ready(function(){
         }]
     });
   });
+  $(document).ready(function(){
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
 
+        var target = this.hash;
+        var $target = $(target);
 
-// $(function () {
-//   var lastScrollTop = 0;
-//   var $navbar = $('.navbar');
-//   var navbarHeight = $navbar.outerHeight();
-//   var movement = 0;
-//   var lastDirection = 0;
-
-//   $(window).scroll(function(event){
-//     var st = $(this).scrollTop();
-//     movement += st - lastScrollTop;
-
-//     if (st > lastScrollTop) { // scroll down
-//       if (lastDirection != 1) {
-//         movement = 0;
-//       }
-//       var margin = Math.abs(movement);
-//       if (margin > navbarHeight) {
-//         margin = navbarHeight;
-//       }
-//       margin = -margin;
-//       $navbar.css('margin-top', margin+"px")
-
-//       lastDirection = 1;
-//     } else { // scroll up
-//       if (lastDirection != -1) {
-//         movement = 0;
-//       }
-//       var margin = Math.abs(movement);
-//       if (margin > navbarHeight) {
-//         margin = navbarHeight;
-//       }
-//       margin = margin-navbarHeight;
-//       $navbar.css('margin-top', margin+"px")
-
-//       lastDirection = -1;
-//     }
-
-//     lastScrollTop = st;
-//     // console.log(margin);
-//   });
-// });
-
-  
+        $('html, body').animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing');
+    });
+});
